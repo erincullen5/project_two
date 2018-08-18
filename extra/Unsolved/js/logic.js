7c42a49d3ea61d5434df0e98aae8e607c79b0182
StevenOrn <stevenorn@aol.com>
2018-08-18 13:59:15 -0500

    commented out some code

--- extra/Unsolved/js/logic.js
+++ extra/Unsolved/js/logic.js
@@ -55,41 +55,41 @@ map.on('zoomend', function() {
 
 
 
-// Building API query URL
-var baseURL = "https://data.cityofnewyork.us/resource/fhrw-4uyv.json?";
-var date = "$where=created_date between'2016-01-10T12:00:00' and '2017-01-01T14:00:00'";
-var complaint = "&complaint_type=Rodent";
-var limit = "&$limit=10000";
+// // Building API query URL
+// var baseURL = "https://data.cityofnewyork.us/resource/fhrw-4uyv.json?";
+// var date = "$where=created_date between'2016-01-10T12:00:00' and '2017-01-01T14:00:00'";
+// var complaint = "&complaint_type=Rodent";
+// var limit = "&$limit=10000";
 
-// Assembling API query URL
-var url = baseURL + date + complaint + limit;
+// // Assembling API query URL
+// var url = baseURL + date + complaint + limit;
 
-// Grabbing the data with d3..
-d3.json(url, function(response) {
+// // Grabbing the data with d3..
+// d3.json(url, function(response) {
 
-  // Creating a new marker cluster group
-    var markers = L.markerClusterGroup({
-        iconCreateFunction: function(cluster) {
-            return L.divIcon({ html: '<b>' + cluster.getChildCount() + '</b>',iconSize:40 });
-        }
-    });
+//   // Creating a new marker cluster group
+//     var markers = L.markerClusterGroup({
+//         iconCreateFunction: function(cluster) {
+//             return L.divIcon({ html: '<b>' + cluster.getChildCount() + '</b>',iconSize:40 });
+//         }
+//     });
 
-  // Loop through our data...
-  for (var i = 0; i < response.length; i++) {
-    // set the data location property to a variable
-    var location = response[i].location;
+//   // Loop through our data...
+//   for (var i = 0; i < response.length; i++) {
+//     // set the data location property to a variable
+//     var location = response[i].location;
 
-    // If the data has a location property...
-    if (location) {
+//     // If the data has a location property...
+//     if (location) {
 
-      // Add a new marker to the cluster group and bind a pop-up
-      markers.addLayer(L.marker([location.coordinates[1], location.coordinates[0]])
-        .bindPopup(response[i].descriptor));
-    }
+//       // Add a new marker to the cluster group and bind a pop-up
+//       markers.addLayer(L.marker([location.coordinates[1], location.coordinates[0]])
+//         .bindPopup(response[i].descriptor));
+//     }
 
-  }
+//   }
 
-  // Add our marker cluster layer to the map
-  map.addLayer(markers);
+//   // Add our marker cluster layer to the map
+//   map.addLayer(markers);
 
-});
+// });
