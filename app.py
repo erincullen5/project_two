9b365f4a0e38f17e65adebfc5fa78133fa4ec73e
Erin Cullen <erin.cullen5@gmail.com>
2018-08-22 18:43:37 -0500

    adding db and updated app.py

--- app.py
+++ app.py
@@ -5,26 +5,29 @@ from flask import(
     request,
     redirect)
 
-import pymysql
-pymysql.install_as_MySQLdb()
-
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 import os
 
+import pymysql
+pymysql.install_as_MySQLdb()
+
+import sqlite3
 
 #create an engine and connect to mysql database
-engine = create_engine("mysql://root:juNEtwentyONE7512475@localhost/fast_food", echo = True)
+# engine = create_engine("mysql://root:juNEtwentyONE7512475@localhost/fast_food", echo = True)
+engine = create_engine("sqlite:///project.db")
 Base = automap_base()
 
 # reflect the tables
 Base.prepare(engine, reflect=True)
 
-Base.classes.keys()
-
-FastFoodTable = Base.classes.fastfoodrestaurants
+fastfood = Base.classes.fastfood
+obesity = Base.classes.obesity
+poverty = Base.classes.poverty
+population = Base.classes.population
 
 # Create our session (link) from Python to the DB
 session = Session(engine)
@@ -37,25 +40,44 @@ app =  Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route("/city")
-def city():
-    results = session.query(FastFoodTable.city).all()
+@app.route("/data/<sample>")
+def byState(sample):
+    results = session.query(Base.classes[sample].state, Base.classes[sample].rate).all()
     return jsonify(results)
 
-@app.route("/data")
-def data():
-    results = session.query(FastFoodTable).all()
-    data = []
-    for r in results:
-        data_dict = {}
-        data_dict["city"] = r.city
-        data_dict["address"] = r.address
-        data_dict["country"] = r.country
-        data_dict["lat"] = str(r.latitude)
-        data_dict["lang"] = str(r.longitude)
-        data_dict["name"] = r.name
-        data.append(data_dict)
-    return jsonify(data)
+# @app.route("/obesity")
+# def obesity():
+#     results = session.query(ObesityTable.state, ObesityTable.rate).all()
+#     data = []
+#     for r in results: 
+#         data_dict = {}
+#         data_dict["state"]= r.state
+#         data_dict["adult_rate"]= str(r.adultObesityRate)
+#         data.append(data_dict)
+#     return jsonify(data)
+
+# @app.route("/poverty")
+# def poverty():
+#     results = session.query(PovertyTable.state, PovertyTable.povertyRate).all()
+#     data = []
+#     for r in results: 
+#         data_dict = {}
+#         data_dict["state"]= r.state
+#         data_dict["rate"]=str(r.povertyRate)
+#         data.append(data_dict)
+#     return jsonify(data)
+
+# @app.route("/ffdata")
+# def data():
+#     results = session.query(FastFoodTable).all()
+#     data = []
+#     for r in results:
+#         data_dict = {}
+#         data_dict["latitude"] = str(r.latitude)
+#         data_dict["longitude"] = str(r.longitude)
+#         data_dict["name"] = r.name
+#         data.append(data_dict)
+#     return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
